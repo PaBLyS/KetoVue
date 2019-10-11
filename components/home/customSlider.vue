dab03ae1442adfad53b87e90cde2ac8c8adad6b7
PaBLyS <pavlobokhan@gmail.com>
2019-10-11 15:58:47 +0300

    create slider on home page

--- components/home/customSlider.vue
+++ components/home/customSlider.vue
@@ -1,37 +1,44 @@
 <template>
   <div>
     <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333; height: 500px; overflow: hidden"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
     >
 
       <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="slider/1.jpg"
-      ></b-carousel-slide>
-
-      <b-carousel-slide img-src="slider/2.jpg">
-        <h1>Hello world!</h1>
+              img-src="/slider/0.jpg"
+      >
+        <h2>INCREASE HYDRATION</h2>
+        <div class="carousel-caption-line"></div>
+        <nuxt-link to="/">SHOP THE COLLECTION</nuxt-link>
       </b-carousel-slide>
 
-      <b-carousel-slide img-src="slider/3.jpg"></b-carousel-slide>
+      <b-carousel-slide
+              img-src="/slider/1.jpg"
+      >
+        <h2>IMPROVE APPEARANCE</h2>
+        <div class="carousel-caption-line"></div>
+        <nuxt-link to="/">SHOP THE COLLECTION</nuxt-link>
+      </b-carousel-slide>
+
+      <b-carousel-slide
+              img-src="/slider/2.jpg"
+      >
+        <h2>REDUCE WRINKLES</h2>
+        <div class="carousel-caption-line"></div>
+        <nuxt-link to="/">SHOP THE COLLECTION</nuxt-link>
+      </b-carousel-slide>
 
     </b-carousel>
   </div>
 </template>
 
 <script>
-
     export default {
         name: "customSlider",
         data() {
@@ -51,6 +58,34 @@
     }
 </script>
 
-<style scoped>
+<style>
+  .carousel-caption {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    right: 15%;
+  }
+
+  .carousel-caption h2{
+    color: #000;
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  .carousel-caption a {
+    color: #fff;
+    font-weight: bold;
+    text-decoration: none;
+    background: #232323;
+    padding: 7px 30px;
+  }
+
+  .carousel-caption-line {
+    margin: 15px auto 30px;
+    width: 40px;
+    background-color: #2e2c2c;
+    height: 3px;
+  }
+
 
 </style>
