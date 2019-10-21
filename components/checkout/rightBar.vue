8be23390ed71448b237b5e29fa514a2c75610f3a
PaBLyS <pavlobokhan@gmail.com>
2019-10-21 11:57:27 +0300

    create rightBar on checkout

--- components/checkout/rightBar.vue
+++ components/checkout/rightBar.vue
@@ -2,23 +2,24 @@
     <div>
         <div class="list">
             <div class="list-item"
-                    v-for="(elem, index) in cart"
-                 v-if="elem.amount">
+                 v-for="(elem, index) in cart"
+                 v-show="elem.amount"
+                 :key="index">
                 <div class="item-info">
                     <div class="img-wrap">
                         <img :src="'/items/' + index + '/1.png'" alt="">
-                        <div>{{ elem.amount}}</div>
+                        <div class="item-index">{{ elem.amount}}</div>
                     </div>
-                    <div>{{ products[index].label }}</div>
+                    <div class="item-name">{{ products[index].label }}</div>
                 </div>
-                <div>${{ products[index].prise }}.00</div>
+                <div class="item-prise">${{ products[index].prise }}.00</div>
             </div>
         </div>
-        <div>
-            <div>Subtotal: <span>${{ totalPrice }}.00</span></div>
-            <div>Shipping <span>Free</span></div>
+        <div class="total-top">
+            <div class="total-top-item"><span>Subtotal:</span><span>${{ totalPrice }}.00</span></div>
+            <div class="total-top-item"><span>Shipping</span><span>Free</span></div>
         </div>
-        <div>Total <span>usd <span>${{ totalPrice }}.00</span></span></div>
+        <div class="total-bottom">Total <span class="total-usd">usd <span class="total-bottom-prise">${{ totalPrice }}.00</span></span></div>
     </div>
 </template>
 
@@ -48,12 +49,35 @@
         height: 65px;
         width: 65px;
         background: #fff;
+        border-radius: 8px;
+        border: 1px solid rgba(0, 0, 0, 0.1);
+        position: relative;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
-    .img-wrap img{
+    .img-wrap img {
         width: 65%;
         height: 65%;
         object-fit: contain;
+    }
+
+    .item-index {
+        position: absolute;
+        top: -10px;
+        right: -10px;
+        padding: 5px;
+        background-color: rgba(114, 114, 114, 0.9);
+        border-radius: 100%;
+        color: #fff;
+        height: 21px;
+        min-width: 21px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-size: 12px;
+        font-weight: 500;
     }
 
     .item-info {
@@ -65,5 +89,55 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+        margin-bottom: 14px;
+    }
+
+    .item-name {
+        padding-left: 14px;
+        color: #323232;
+        font-size: 14px;
+        font-weight: 500;
+    }
+
+    .item-prise {
+        color: #323232;
+        font-weight: 500;
+        font-size: 14px;
+    }
+
+    .total-top {
+        border-top: 1px solid  rgba(175,175,175,0.34);
+        border-bottom: 1px solid  rgba(175,175,175,0.34);
+        padding: 21px 0 11px;
+        font-size: 14px;
+    }
+
+    .total-top-item {
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 11px;
+    }
+
+    .total-bottom {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 21px;
+        font-size: 16px;
+    }
+    
+    .total-usd {
+        display: flex;
+        align-items: center;
+        text-transform: uppercase;
+        font-size: 12px;
+        color: #717171;
+    }
+
+    .total-bottom-prise {
+        color: #323232;
+        font-weight: 500;
+        font-size: 24px;
+        margin-left: 10px;
     }
 </style>
