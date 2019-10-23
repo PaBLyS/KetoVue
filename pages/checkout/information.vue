2219cefff2333c727c32b45b909f66e521431a6c
PaBLyS <pavlobokhan@gmail.com>
2019-10-23 17:18:41 +0300

    add button on form

--- pages/checkout/information.vue
+++ pages/checkout/information.vue
@@ -65,6 +65,10 @@
             <custom-checkbox nameCheckbox="save"
                              textCheckbox="Save this information for next time"/>
         </div>
+        <div class="wrap-button">
+            <nuxt-link to="/cart" class="button-back">Return to cart</nuxt-link>
+            <nuxt-link to="/" class="button-next">Continue to shipping</nuxt-link>
+        </div>
     </section>
 </template>
 
@@ -107,5 +111,54 @@
         margin: 21px 6px;
         font-size: 18px;
         font-weight: 300;
+    }
+
+    .wrap-button {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .button-next {
+        font-size: 14px;
+        font-weight: 500;
+        color: #fff;
+        background: #000;
+        padding: 20px 24px;
+        margin-right: 6px;
+        border-radius: 5px;
+    }
+    
+    .button-next:hover{
+        text-decoration: none;
+    }
+
+    .button-back {
+        color: #2d2d2d;
+        font-size: 14px;
+        font-weight: 400;
+        position: relative;
+        padding-left: 10px;
+    }
+
+    .button-back:hover {
+        text-decoration: none;
+    }
+
+    .button-back:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 35%;
+        height: 8px;
+        width: 8px;
+        border-bottom: 1px solid #232323;
+        border-left: 1px solid #232323;
+        transform: rotate(45deg);
+        transition: left .3s ease-in;
+    }
+
+    .button-back:hover:after {
+        left: -5px;
     }
 </style>
